0b055eceded447639dba2565016d2260af5b25c5
gilson657 <gilsonlf.24@gmail.com>
2025-02-03 11:19:35 -0400

    Projeto site

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Exemplo de script personalizado (opcional)
+console.log("Página carregada com sucesso!");
+
+
+window.onload = function () {
+  alert("Seja bem-vindo ao site da MasterSoluções! Explore nossas soluções e serviços.");
+};
